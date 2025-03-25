10375cc6509a3a7a81e5d30637f8b5c23a16f7cb
BienBeo <tranngocbien2809@gmail.com>
2025-03-25 17:20:10 +0700

    update JDK version & UI Dashboard

--- FacilityUI/src/main/java/module-info.java
+++ FacilityUI/src/main/java/module-info.java
@@ -1,19 +1,23 @@
 module com.utc2.facilityui {
-    requires javafx.controls;
     requires javafx.fxml;
     requires javafx.web;
 
     requires org.controlsfx.controls;
     requires com.dlsc.formsfx;
     requires net.synedra.validatorfx;
-    requires org.kordamp.ikonli.javafx;
     requires org.kordamp.bootstrapfx.core;
-    requires eu.hansolo.tilesfx;
     requires okhttp3;
     requires com.google.gson;
+    requires de.jensd.fx.glyphs.fontawesome;
+    requires javafx.controls;
+    requires javafx.graphics;
+    requires com.gluonhq.attach.util;
+    requires com.gluonhq.charm.glisten;
 
     exports com.utc2.facilityui.controller;
     opens com.utc2.facilityui.controller to javafx.fxml;
     exports com.utc2.facilityui.app;
     opens com.utc2.facilityui.app to javafx.fxml;
+    opens com.utc2.facilityui.service to javafx.fxml;
+    opens com.utc2.facilityui.view to javafx.fxml;
 }
