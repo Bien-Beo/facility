11b6adec618acfe827a54da1ee490a0459445ad1
BienBeo <tranngocbien2809@gmail.com>
2025-03-28 21:57:37 +0700

    Update DashbroadDashbroad

---
+++ webclient/src/pages/DashboardPage.tsx
@@ -0,0 +1,12 @@
+import { FC, JSX } from "react";
+import Facilities from "../components/Facilities";
+
+const DashboardPage: FC = (): JSX.Element => {
+  return (
+    <div className="w-full h-full flex">
+      <Facilities />
+    </div>
+  );
+};
+
+export default DashboardPage;
